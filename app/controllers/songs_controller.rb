5cf88f06326ac82fc4a1c2f6b0d81efc0ed121e4
+ <+>
2021-07-01 20:24:28 -0700

    Set up nested resources for artists and songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,29 @@
 class SongsController < ApplicationController
+  
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      if Artist.find_by_id(params[:artist_id])
+        @songs = Artist.find(params[:artist_id]).songs
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      if Song.find_by_id(params[:id]) && Song.find_by_id(params[:id]).artist == Artist.find_by_id(params[:artist_id])
+        @song = Artist.find(params[:artist_id]).songs.find(params[:id])
+      else
+        flash[:alert] = "Song not found"
+        redirect_to artist_songs_path
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
